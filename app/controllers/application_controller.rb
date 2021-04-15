a5acb874a3639e2e32d7c0522a2efe054b09e374
Morenikeji Bello <caeserselassie@gmail.com>
2021-04-15 23:36:23 +0100

    Separated user's events into upcoming and previous

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,16 @@ class ApplicationController < ActionController::Base
     end
 
     def authenticate_user
+        other_user = User.find(params['id'])
+
         if !user_logged_in?
           flash[:alert] = 'You must sign in to continue'
-          redirect_to new_user_path
+          redirect_to users_sign_in_path
+        elsif current_user != other_user
+            flash[:alert] = 'You cannot access that page'
+            redirect_to current_user
         end
+
     end
 
     def current_user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     before_action :authenticate_user, only: [:show]
     before_action :current_user, only: [:show]
+    # before_action :set_user, only: [:show]
     def new
       @user = User.new
     end
@@ -37,6 +38,7 @@ class UsersController < ApplicationController
 
     def show
       @user_events = @user.events
+      @user_attending_events = @user.attending_events
     end
 
     private
@@ -44,4 +46,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name)
     end
+
 end
