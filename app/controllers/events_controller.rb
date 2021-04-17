c9d6ba9d8ba8ec6591cd805451b13c9667d9a8bf
Morenikeji Bello <caeserselassie@gmail.com>
2021-04-17 14:35:38 +0100

    Change user class variable to current_user

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,7 +10,7 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = @user.events.build(event_params)
+    @event = @current_user.events.build(event_params)
     if @event.save
       flash[:notice] = 'Event created Successfully'
       redirect_to @event
